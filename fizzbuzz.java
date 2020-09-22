70b03e29955de925467e577633f67770f78e9567
Dinesh72 <42167469+Dinesh72@users.noreply.github.com>
2020-09-23 01:03:58 +0530

    Update fizzbuzz.java

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -1,4 +1,4 @@
-//Refactor
+//refactor
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -13,64 +13,70 @@ class  fizzbuzz{
 
     public static boolean contains_3(int number)
     {
-     if(number%10==3)
-         return true;
-         else
-         return false;
+        return ((number%10==3?true:false));
+//     if(number%10==3)
+//         return true;
+//         else
+//         return false;
     }
     public static boolean contains_5(int number)
     {
-     if(number%10==5)
-         return true;
-         else
-         return false;
+        return ((number%10==5?true:false));
+//     if(number%10==5)
+//         return true;
+//         else
+//         return false;
     }
     public static boolean isDivisibleby_3and5(int number)
 {
-    Boolean status=false;
-    Integer checknumber;
-   if(number%3==0 && number%5==0)
-   {
-       status=true;
-   }
-   else
-   {
-       status=false;
-   }
-    
-    return status;
+    return ((number%3==0) && (number%5==0) ?true:false);
+//    Boolean status=false;
+//    Integer checknumber;
+//   if(number%3==0 && number%5==0)
+//   {
+//       status=true;
+//   }
+//   else
+//   {
+//       status=false;
+//   }
+//    
+//    return status;
     
 }
 public static boolean isDivisibleby_3(int number)
 {
-    Boolean status=false;
-    Integer checknumber;
-   if(number%3==0)
-   {
-       status=true;
-   }
-   else
-   {
-       status=false;
-   }
-    
-    return status;
+    return ((number%3==0?true:false));
+//    Boolean status=false;
+//    Integer checknumber;
+//   if(number%3==0)
+//   {
+//       status=true;
+//   }
+//   else
+//   {
+//       status=false;
+//   }
+//    
+//    return status;
     
 }
 public static boolean isDivisibleby_5(int number)
 {
-    Boolean status=false;
-    Integer checknumber;
-   if(number%5==0)
-   {
-       status=true;
-   }
-   else
-   {
-       status=false;
-   }
-    
-    return status;
-    
+    return ((number%5==0?true:false));
+//    Boolean status=false;
+//    Integer checknumber;
+//   if(number%5==0)
+//   {
+//       status=true;
+//   }
+//   else
+//   {
+//       status=false;
+//   }
+//    
+//    return status;
+//    
 }
 }
+
