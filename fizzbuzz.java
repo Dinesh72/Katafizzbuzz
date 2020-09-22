87ff2f474ea6d9a35c54e3be7b63781a71a172b5
Dinesh72 <42167469+Dinesh72@users.noreply.github.com>
2020-09-23 01:02:56 +0530

    Update fizzbuzz.java

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -1,7 +1,33 @@
+///3 and 5
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-////divisible 5 
+/**
+ *
+ * @author pravin
+ */
 class  fizzbuzz{
 
+  
+    public boolean isDivisibleby_3and5(int number)
+{
+    Boolean status=false;
+    Integer checknumber;
+   if(number%3==0 && number%5==0)
+   {
+       status=true;
+   }
+   else
+   {
+       status=false;
+   }
+    
+    return status;
+    
+}
 public boolean isDivisibleby_3(int number)
 {
     Boolean status=false;
