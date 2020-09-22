94729682c59ffef2e08eedfe434f54949ad95423
Dinesh72 <42167469+Dinesh72@users.noreply.github.com>
2020-09-23 01:01:08 +0530

    Create fizzbuzz.java

---
+++ fizzbuzz.java
@@ -0,0 +1,20 @@
+//divisible by 3 
+class  fizzbuzz{
+
+public boolean isDivisibleby_3(int number)
+{
+    Boolean status=false;
+    Integer checknumber;
+   if(number%3==0)
+   {
+       status=true;
+   }
+   else
+   {
+       status=false;
+   }
+    System.out.println("Number "+i)
+    return status;
+    
+}
+}
