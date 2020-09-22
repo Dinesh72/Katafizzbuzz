e67928665e82e7cbe851389ac96a5e8d711d3939
Dinesh72 <42167469+Dinesh72@users.noreply.github.com>
2020-09-23 01:05:14 +0530

    Update katafizzbuzzTest.java

--- katafizzbuzzTest.java
+++ katafizzbuzzTest.java
@@ -1,33 +1,82 @@
-stage 1
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-///divisible by 3
-
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
-public class katafizzbuzz {
+/**
+ *
+ * @author pravin
+ */
+public class katafizzbuzzTest {
     
     
 
     @Test
      public void fizzbuzztest() {
-     fizzbuzz test1=new fizzbuzz();
-     String checknumber=null;
-     String Status;
+//     fizzbuzz test1=new fizzbuzz();
+     
+     
      for(int i=1;i<=100;i++)
      {
+         
          try{
-        Assert.assertTrue(test1.isDivisibleby_3(i));
+        Assert.assertTrue(fizzbuzz.isDivisibleby_3and5(i));
+        System.out.println("FizzBuzz");
          }
          catch(AssertionError e)
          {
+             
+              try{
+        Assert.assertTrue(fizzbuzz.contains_3(i));
+        System.out.println("Fizz");
+         }
+         catch(AssertionError e4)
+         {
+            try{
+        Assert.assertTrue(fizzbuzz.contains_5(i));
+        System.out.println("Buzz");
+         }
+         catch(AssertionError e5)
+         {
+                
+           try{
+               
+        Assert.assertTrue(fizzbuzz.isDivisibleby_3(i));
+        System.out.println("Fizz");
+         }
+         catch(AssertionError e2)
+         {
+             try{
+               
+        Assert.assertTrue(fizzbuzz.isDivisibleby_5(i));
+        System.out.println("Buzz");
+         }
+         catch(AssertionError e3)
+         {
              System.out.println(i);
          }
-         System.out.println("Fizz");
-        
-      
+         }
+         }
+         
+         
+         }
          
      }
 
     }
+     }
 }
+
+
+
+
+    
