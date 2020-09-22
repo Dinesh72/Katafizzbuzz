52ab026cf4bdb0e32de243877ac66b9a34177a44
Dinesh72 <42167469+Dinesh72@users.noreply.github.com>
2020-09-23 01:04:40 +0530

    Update fizzbuzz.java

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -1,4 +1,3 @@
-//refactor
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -7,76 +6,34 @@
 
 /**
  *
- * @author Dinesh
+ * @author pravin
  */
 class  fizzbuzz{
 
     public static boolean contains_3(int number)
     {
         return ((number%10==3?true:false));
-//     if(number%10==3)
-//         return true;
-//         else
-//         return false;
+
     }
     public static boolean contains_5(int number)
     {
         return ((number%10==5?true:false));
-//     if(number%10==5)
-//         return true;
-//         else
-//         return false;
+
     }
     public static boolean isDivisibleby_3and5(int number)
 {
     return ((number%3==0) && (number%5==0) ?true:false);
-//    Boolean status=false;
-//    Integer checknumber;
-//   if(number%3==0 && number%5==0)
-//   {
-//       status=true;
-//   }
-//   else
-//   {
-//       status=false;
-//   }
-//    
-//    return status;
-    
+
 }
 public static boolean isDivisibleby_3(int number)
 {
     return ((number%3==0?true:false));
-//    Boolean status=false;
-//    Integer checknumber;
-//   if(number%3==0)
-//   {
-//       status=true;
-//   }
-//   else
-//   {
-//       status=false;
-//   }
-//    
-//    return status;
+
     
 }
 public static boolean isDivisibleby_5(int number)
 {
     return ((number%5==0?true:false));
-//    Boolean status=false;
-//    Integer checknumber;
-//   if(number%5==0)
-//   {
-//       status=true;
-//   }
-//   else
-//   {
-//       status=false;
-//   }
-//    
-//    return status;
-//    
-}
-}
 
+}
+}
